b0b31bef961960cbf3ed27333d142b76ed69d7d7
Albin Hedman <albin9604@gmail.com>
2021-03-01 20:05:50 +0100

    Add examples

---
+++ examples/matrix.rs
@@ -0,0 +1,37 @@
+#![allow(incomplete_features)]
+#![feature(const_generics)]
+#![feature(const_evaluatable_checked)]
+
+extern crate iter_fixed;
+
+use iter_fixed::IntoIteratorFixed;
+
+#[derive(Clone, Copy)]
+struct Matrix<T, const R: usize, const K: usize> {
+    rows: [[T; K]; R],
+}
+
+impl<T, const R: usize, const K: usize> Matrix<T, R, K> {
+    fn get_col(&self, col_id: usize) -> [&T; R] {
+        (&self.rows)
+            .into_iter_fixed()
+            .map(|row| &row[col_id])
+            .collect()
+    }
+
+    fn get_row(&self, row_id: usize) -> [&T; K] {
+        (&self.rows[row_id]).into_iter_fixed().collect()
+    }
+}
+
+fn main() {
+    let m = Matrix {
+        rows: [
+            [1, 2, 3],
+            [4, 5, 6]
+        ],
+    };
+
+    assert_eq!(m.get_row(0), [&1, &2, &3]);
+    assert_eq!(m.get_col(0), [&1, &4]);
+}
---
+++ examples/vector.rs
@@ -0,0 +1,61 @@
+#![allow(incomplete_features)]
+#![feature(const_generics)]
+#![feature(const_evaluatable_checked)]
+
+extern crate iter_fixed;
+
+use iter_fixed::IntoIteratorFixed;
+use std::ops::{Add, Mul};
+
+#[derive(Clone, Copy)]
+struct Vector<T, const N: usize> {
+    elements: [T; N],
+}
+
+impl<T, const N: usize> Vector<T, N>
+where
+    T: Mul<T, Output = T> + std::iter::Sum,
+{
+    fn new(elements: [T; N]) -> Self {
+        Vector { elements }
+    }
+}
+
+impl<T, const N: usize> Vector<T, N>
+where
+    T: Mul<T, Output = T> + std::iter::Sum + Clone,
+{
+    fn length2(self) -> T {
+        self.elements
+            .into_iter_fixed()
+            .into_iter()
+            .map(|x| x.clone() * x)
+            .sum()
+    }
+}
+
+impl<A, B, const N: usize> Add<Vector<B, N>> for Vector<A, N>
+where
+    A: Add<B>,
+{
+    type Output = Vector<<A as Add<B>>::Output, N>;
+
+    fn add(self, other: Vector<B, N>) -> Self::Output {
+        Vector {
+            elements: self
+                .elements
+                .into_iter_fixed()
+                .zip(other.elements)
+                .map(|(a, b)| a + b)
+                .collect(),
+        }
+    }
+}
+
+fn main() {
+    let a = Vector::new([1, 2, 3]);
+    let b = Vector::new([1, 1, 1]);
+
+    assert_eq!((a + b).elements, [2, 3, 4]);
+    assert_eq!(b.length2(), 3);
+}
